36113413f51bd11f349d9fc4a530d9341549bc7a
santiago <72000936+santiagov916@users.noreply.github.com>
2021-11-26 18:28:28 -0800

    prompt user for more input

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-// !!! the beginning text to take in data and pass it through to console it
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-const { profile } = require('console');
+// // !!! the beginning text to take in data and pass it through to console it
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+// profileDataArgs = process.argv.slice(2);
 
-// call the fs module 
-const fs = require('fs');
+// // ES6 feature: assignment destructuring
+// const [name, github] = profileDataArgs;
 
-// call the page-template
-const generatePage = require('./src/page-template.js')
-
-profileDataArgs = process.argv.slice(2);
-
-// ES6 feature: assignment destructuring
-const [name, github] = profileDataArgs;
+// const { profile } = require('console');
 
 
-// call the fs method and insert three arguments : file name, where the data is coming from, and a call back (error) as the theird
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+const inquirer = require('inquirer');
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+// // call the fs module 
+// const fs = require('fs');
+
+// // call the page-template
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// // call the fs method and insert three arguments : file name, where the data is coming from, and a call back (error) as the theird
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
+
+
 
             // REFERENCE CODE !! //
 // const printProfileData = profileDataArr => {
